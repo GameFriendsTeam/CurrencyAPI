08950ac982b67dfd7ce9000e40af281307f283f9
Savin1play140 <savingame1140@gmail.com>
2026-06-27 19:03:03 +0300

    Compile prepare

---
+++ compiler.php
@@ -0,0 +1,7 @@
+<?php
+if (is_dir('build/')) {
+    mkdir('build/');
+}
+$phar = new Phar("build/Currency.phar");
+$phar->buildFromDirectory(__DIR__, '/^(?!.*vendor).*$/');
+$phar->setStub('<?php __HALT_COMPILER();');
